687105a89c9841da1e1afab590f058046d33450b
Kengo <ikedaiyk@newschool.edu>
2023-02-03 14:21:47 -0500

    Created `/assets` directory.
    Updated head assets in each index file.

---
+++ cis23/assets/script.js
@@ -0,0 +1,8 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+    console.log("I don't know any jokes.");
+
+    function bless_you() {
+      alert("bless you!")
+    }
